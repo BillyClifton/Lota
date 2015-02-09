e68de9727911b61629c477edf97b78a75126be91
Ubuntu <ubuntu@ip-172-31-41-195.ec2.internal>
2015-02-09 02:38:17 +0000

    fixex comments

--- routes/index.js
+++ routes/index.js
@@ -38,9 +38,7 @@ router.param('post', function(req, res, next, id) {
     return next();
   });
 });
-router.get('/posts/:post', function(req, res) {
-  res.json(req.post);
-});
+
 
 router.put('/posts/:post/upvote', function(req, res, next) {
   req.post.upvote(function(err, post){
@@ -63,6 +61,18 @@ router.post('/posts/:post/comments', function(req, res, next) {
 
       res.json(comment);
     });
+  });
+});
+
+router.param('comment', function(req, res, next, id) {
+  var query = Comment.findById(id);
+
+  query.exec(function (err, comment){
+    if (err) { return next(err); }
+    if (!comment) { return next(new Error('can\'t find comment')); }
+
+    req.comment = comment;
+    return next();
   });
 });
 router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
